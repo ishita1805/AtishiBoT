13130ed3b9815dfc1a8fb96b009860edfb8f56c6
Ishita Kabra <ishitakabra1805@gmail.com>
2021-03-31 00:30:47 +0530

    web UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,40 @@
-console.log('connected');
+
 var header = document.querySelector("#navigation");
 var btns = header.querySelectorAll(".nav-item");
-console.log(btns);
+
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
+
+    var current2 = document.getElementsByClassName("active2");
+    current2[0].className = current2[0].className.replace(" active2", "");
+    this.className += " active2";
+
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+
+  });
+}
+
+var btns2 = header.querySelectorAll(".nav-item2");
+
+for (var i = 0; i < btns2.length; i++) {
+  btns2[i].addEventListener("click", function() {
+
+  var current2 = document.getElementsByClassName("active2");
+  current2[0].className = current2[0].className.replace(" active2", "");
+  this.className += " active2";
+
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
   this.className += " active";
   });
 }
+
+
+$(document).ready(function(){
+  $("#navbutton").click(function(){
+    $("#navtoggle").slideToggle("slow");
+    console.log(document.querySelector('#navtoggle').style.display);
+  });
+});
